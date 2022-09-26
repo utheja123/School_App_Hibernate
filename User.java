d5c59f94851f8fa2a89d21199ace23b796d8e3db
utheja123 <114225755+utheja123@users.noreply.github.com>
2022-09-26 20:09:48 +0530

    Add files via upload
    userdto added

---
+++ User.java
@@ -0,0 +1,54 @@
+package com.ty.school.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name="details_of_users")
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column(name="emailid",unique=true)
+	private String email;
+	@Column(nullable=false)
+	private String password;
+	private String gender;
+	private long phone;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	
+
+}
